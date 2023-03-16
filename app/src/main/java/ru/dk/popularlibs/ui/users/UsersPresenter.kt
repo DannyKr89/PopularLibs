96ba194147d832200676ece3e5c811f75d427af1
dk <omnipresent9@yandex.ru>
2023-03-16 22:34:53 +0300

    Добаил RxJava3

--- app/src/main/java/ru/dk/popularlibs/ui/users/UsersPresenter.kt
+++ app/src/main/java/ru/dk/popularlibs/ui/users/UsersPresenter.kt
@@ -1,19 +1,26 @@
 package ru.dk.popularlibs.ui.users
 
+import android.annotation.SuppressLint
+import io.reactivex.rxjava3.kotlin.subscribeBy
 import moxy.MvpPresenter
 import ru.dk.popularlibs.domain.GithubUsersRepo
 
-class UsersPresenter(val usersRepo: GithubUsersRepo) : MvpPresenter<UsersView>() {
+class UsersPresenter(private val usersRepo: GithubUsersRepo) : MvpPresenter<UsersView>() {
 
 
+    @SuppressLint("CheckResult")
     fun loadData() {
         viewState.showProgressbar(true)
-        usersRepo.getUsers(onSuccess = {
-            viewState.showUsers(it)
-            viewState.showProgressbar(false)
-        }, onError = {
-            viewState.showError(it)
-            viewState.showProgressbar(false)
-        })
+        usersRepo.getUsers()
+            .subscribeBy(
+                onSuccess = {
+                    viewState.showUsers(it)
+                    viewState.showProgressbar(false)
+                },
+                onError = {
+                    viewState.showError(it)
+                    viewState.showProgressbar(false)
+                }
+            )
     }
 }
